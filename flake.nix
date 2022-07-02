78875fa9ee21d3e88a6203e67d2f9ed2aa994908
hugosenari <hugosenari@gmail.com>
2022-07-02 03:22:43 -0300

    chore: re index html5_canvas

--- flake.nix
+++ flake.nix
@@ -7,14 +7,6 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
-  inputs."html5_canvas-master".dir   = "master";
-  inputs."html5_canvas-master".owner = "nim-nix-pkgs";
-  inputs."html5_canvas-master".ref   = "master";
-  inputs."html5_canvas-master".repo  = "html5_canvas";
-  inputs."html5_canvas-master".type  = "github";
-  inputs."html5_canvas-master".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."html5_canvas-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
